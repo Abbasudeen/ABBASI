2a47967e7c4026ba7b667af988e8c44efe535e83
Abbasudeen <42763410+Abbasudeen@users.noreply.github.com>
2018-09-24 13:55:03 +0530

    Create prntntimes.java

---
+++ prntntimes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main 
+{
+  public static void main(String[] args)
+   {
+     int n;
+     System.out.print("enter N");
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     for (int i = 0; i < n; i++)
+      {
+      System.out.println("Hello World");
+      }
+  }
+}
