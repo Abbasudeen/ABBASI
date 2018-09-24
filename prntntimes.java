d3b753c6cd23f85508b688689c6ca4a46051942c
Abbasudeen <42763410+Abbasudeen@users.noreply.github.com>
2018-09-24 13:57:31 +0530

    Update prntntimes.java

--- prntntimes.java
+++ prntntimes.java
@@ -4,7 +4,7 @@ public class Main
   public static void main(String[] args)
    {
      int n;
-     System.out.print("enter N");
+     System.out.print("");
      Scanner s=new Scanner(System.in);
      n=s.nextInt();
      for (int i = 0; i < n; i++)
