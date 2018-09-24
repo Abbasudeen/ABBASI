ae443b54648d8583fc971b89985bd5c353470161
Abbasudeen <42763410+Abbasudeen@users.noreply.github.com>
2018-09-24 14:27:19 +0530

    Create naturalnum.java

---
+++ naturalnum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args)
+   {
+     int b=0,a,i;
+     Scanner s=new Scanner(System.in);
+     a=s.nextInt();
+     for(i=0;i<=a;i++)
+     {
+        b=b+i;
+     }
+       System.out.println(b);
+  }
+}
