ecc2b1eff98ddc12e4a2a21060dadd5f1fa2daef
Abbasudeen <42763410+Abbasudeen@users.noreply.github.com>
2018-09-25 08:59:52 +0530

    Create nk.java

---
+++ nk.java
@@ -0,0 +1,25 @@
+public class kn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+
+     int n,k;
+       int[] sr; 
+     int sum=0;
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     k=s.nextInt();
+     sr = new int[n];
+     for(int i=0;i<n;i++)
+     {
+     	sr[i]=s.nextInt();
+     }
+     
+     for(int i=0;i<k;i++)
+     {
+     	sum=sum+sr[i];
+     }
+     System.out.println(+sum);
+     }
+}
