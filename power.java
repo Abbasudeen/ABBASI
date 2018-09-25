4708794c98427e45b1ab4b5b04da84ce450448a3
Abbasudeen <42763410+Abbasudeen@users.noreply.github.com>
2018-09-25 09:15:05 +0530

    Update power.java

--- power.java
+++ power.java
@@ -3,16 +3,16 @@ public class Power
 {
   public static void main(String[] args) 
   {
-      int a,b,r=1;
+      int a,b,s=1;
       Scanner s=new Scanner (System.in);
       a=s.nextInt();
       b=s.nextInt();
      {
          for(int i=1;i<=b;i++)
 	      {
-            r=r*a;
+            s=s*a;
  	      }	    
      }
-    System.out.println(r);
+    System.out.println(s);
   } 
 }
